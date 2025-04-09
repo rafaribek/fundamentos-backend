b3183bef9dec7aafe2abbf63f6a4072b040d2c97
Rafael <rafael.2023014116@alunocesul.com>
2025-04-08 22:47:39 -0300

    Regex

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from './pipes/zod-validation-pipe';
 
-@Controller()
+const createProductBodySchema = z.object({
+  name: z.string().min(3).max(20),
+  model: z.string(),
+  dateManufacture: z.string(),
+  year: z.string(),
+  brand: z.string(),
+  email: z.string().email(),
+  cpf: z.string().regex(new RegExp(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/),"CPF inválido")
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createProductBodySchema);
+
+type CreateProductBodySchema = z.infer<typeof createProductBodySchema>;
+
+@Controller('/products')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Post()
+  create(@Body(bodyValidationPipe) body: CreateProductBodySchema): string {
+    return "create";
+  }   
 }
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { unknown, ZodError, ZodSchema } from "zod";
+import { fromZodError } from "zod-validation-error";
+
+export class ZodValidationPipe implements PipeTransform {
+    constructor(private schema: ZodSchema){}
+    
+    transform(value: unknown) {
+    try{
+        return this.schema.parse(value);
+    }catch (error) {
+        if(error instanceof ZodError) {
+            throw new BadRequestException({
+                message: 'Validation failed',
+                statusCode: 400,
+                errors: fromZodError(error),
+            }); 
+        }
+    }
+        return value;
+}
+
+}
