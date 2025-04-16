1e674baee560dcb5063eeea08689147bc886ae0f
Rafael <rafael.2023014116@alunocesul.com>
2025-04-15 22:12:23 -0300

    Post, Put, Patch

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,24 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from './pipes/zod-validation-pipe';
+
+function isValidCPF(cpf: string): boolean {
+  cpf = cpf.replace(/[^\d]+/g, '');
+
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let sum = 0;
+  for (let i = 0; i < 9; i++) sum += +cpf.charAt(i) * (10 - i);
+  let rev = 11 - (sum % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  if (rev !== +cpf.charAt(9)) return false;
+
+  sum = 0;
+  for (let i = 0; i < 10; i++) sum += +cpf.charAt(i) * (11 - i);
+  rev = 11 - (sum % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  return rev === +cpf.charAt(10);
+}
 
 const createProductBodySchema = z.object({
   name: z.string().min(3).max(20),
@@ -9,19 +27,77 @@ const createProductBodySchema = z.object({
   year: z.string(),
   brand: z.string(),
   email: z.string().email(),
-  cpf: z.string().regex(new RegExp(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/),"CPF inválido")
+  cpf: z.string()
+  .regex(/^\d{11}$/, {
+    message: 'CPF deve conter exaamente 11 digitos numéricos',
+  })
+  .refine(isValidCPF, {
+    message: "CPF invalid"
+  })
 })
 
 const bodyValidationPipe = new ZodValidationPipe(createProductBodySchema);
 
 type CreateProductBodySchema = z.infer<typeof createProductBodySchema>;
 
+const updateProductBodySchema = z.object({
+  name: z.string().min(3).max(20).optional(),
+  model: z.string().optional(),
+  dateManufacture: z.string().optional(),
+  year: z.string().optional(),
+  brand: z.string(),
+  email: z.string().email(),
+  cpf: z.string()
+  .regex(/^\d{11}$/, {
+    message: 'CPF deve conter exaamente 11 digitos numéricos',
+  })
+  .refine(isValidCPF, {
+    message: "CPF invalid"
+  }).optional()
+})
+
+const updateBodyValidationPipe = new ZodValidationPipe(updateProductBodySchema);
+
+type UpdateProductBodySchema = z.infer<typeof updateProductBodySchema>;
+
+
 @Controller('/products')
 export class AppController {
   constructor() {}
 
   @Post()
-  create(@Body(bodyValidationPipe) body: CreateProductBodySchema): string {
-    return "create";
+  @HttpCode(201)
+  create(@Body(bodyValidationPipe) body: CreateProductBodySchema) {
+    const { brand, dateManufacture, cpf, email, model, name, year } = body;
+
+    // Executar o service com a regra de negócio
+    
   }   
+  @Get()
+  findAll(){
+
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    
+  }
+
+  @Put(':id')
+  update(@Body(updateProductBodySchema) body: UpdateProductBodySchema){
+    const { brand, dateManufacture, cpf, email, model, name, year } = body;
+
+  }
+
+  @Patch(':id/status')
+  @HttpCode(204)
+  updateStatus(
+    @Param('id') id: string,
+    @Body(updateProductBodySchema) body: UpdateProductBodySchema,
+  ) {
+    const { brand, dateManufacture, cpf, email, model, name, year } = body;
+
+  }
+  
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 
+
 @Module({
   imports: [],
   controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
